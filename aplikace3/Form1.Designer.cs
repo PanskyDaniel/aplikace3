0f8bbab34ccc1cd2fc10a93b7b59b27883dee01c
Daniel Panský <daniel.pansky@centrum.cz>
2023-05-16 12:13:44 +0200

    Přidejte soubory projektu.

---
+++ aplikace3/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Aplikace3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SMTP = new System.Windows.Forms.TextBox();
+            this.Port = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SSL = new System.Windows.Forms.CheckBox();
+            this.Odesilatel = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Prijemce = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TextZpravy = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Predmet = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SMTP
+            // 
+            this.SMTP.Location = new System.Drawing.Point(89, 31);
+            this.SMTP.Name = "SMTP";
+            this.SMTP.Size = new System.Drawing.Size(323, 22);
+            this.SMTP.TabIndex = 0;
+            // 
+            // Port
+            // 
+            this.Port.Location = new System.Drawing.Point(89, 64);
+            this.Port.Name = "Port";
+            this.Port.Size = new System.Drawing.Size(52, 22);
+            this.Port.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "SMPT:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 177);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Uložit nastavení";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SSL
+            // 
+            this.SSL.AutoSize = true;
+            this.SSL.Location = new System.Drawing.Point(89, 134);
+            this.SSL.Name = "SSL";
+            this.SSL.Size = new System.Drawing.Size(140, 21);
+            this.SSL.TabIndex = 3;
+            this.SSL.Text = "SSL zabezpečení";
+            this.SSL.UseVisualStyleBackColor = true;
+            // 
+            // Odesilatel
+            // 
+            this.Odesilatel.Location = new System.Drawing.Point(89, 95);
+            this.Odesilatel.Name = "Odesilatel";
+            this.Odesilatel.Size = new System.Drawing.Size(323, 22);
+            this.Odesilatel.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Odesílatel:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(690, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 26);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Odeslat e-mail";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Prijemce
+            // 
+            this.Prijemce.Location = new System.Drawing.Point(606, 28);
+            this.Prijemce.Name = "Prijemce";
+            this.Prijemce.Size = new System.Drawing.Size(326, 22);
+            this.Prijemce.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(534, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Příjemce:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // TextZpravy
+            // 
+            this.TextZpravy.Location = new System.Drawing.Point(606, 98);
+            this.TextZpravy.Margin = new System.Windows.Forms.Padding(4);
+            this.TextZpravy.Multiline = true;
+            this.TextZpravy.Name = "TextZpravy";
+            this.TextZpravy.Size = new System.Drawing.Size(326, 129);
+            this.TextZpravy.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(534, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Zpráva";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Predmet
+            // 
+            this.Predmet.Location = new System.Drawing.Point(606, 64);
+            this.Predmet.Margin = new System.Windows.Forms.Padding(4);
+            this.Predmet.Name = "Predmet";
+            this.Predmet.Size = new System.Drawing.Size(326, 22);
+            this.Predmet.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(534, 67);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Předmět";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 447);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Predmet);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TextZpravy);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Prijemce);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Odesilatel);
+            this.Controls.Add(this.SSL);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Port);
+            this.Controls.Add(this.SMTP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SMTP;
+        private System.Windows.Forms.TextBox Port;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox SSL;
+        private System.Windows.Forms.TextBox Odesilatel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox Prijemce;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TextZpravy;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Predmet;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ aplikace3/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.IO;
+
+namespace Aplikace3
+{
+    public partial class Form1 : Form
+    {
+        private const string nazevSouboru = "pokus.ini";
+        private string soubor;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string exe = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string cesta = Path.GetDirectoryName(exe);
+            soubor = Path.Combine(cesta, nazevSouboru);
+
+            if (!File.Exists(soubor))
+            {
+                try
+                {
+                    File.Create(soubor);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Chyba při vytvoření souboru", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Close();
+                }
+            }
+
+            IniFile ini = new IniFile(soubor);
+
+            // Reading from INI file
+            SMTP.Text = ini.Read("SMTP", "Sekce1");
+            Port.Text = ini.Read("PORT", "Sekce1");
+            SSL.Checked = ini.Read("SSL", "Sekce1") == "True";
+
+            Odesilatel.Text = ini.Read("Odesilatel", "Sekce2");
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IniFile ini = new IniFile(soubor);
+
+            // Writing to INI file
+            ini.Write("SMTP", SMTP.Text, "Sekce1");
+            ini.Write("PORT", Port.Text, "Sekce1");
+            ini.Write("SSL", SSL.Checked.ToString(), "Sekce1");
+            ini.Write("Odesilatel", Odesilatel.Text, "Sekce2");
+
+            MessageBox.Show("Nastavení bylo uloženo", "Nastavení bylo uloženo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(SMTP.Text))
+            {
+                MessageBox.Show("Hodnota SMPT musí být vyplněna!");
+                return;
+            }
+            if (string.IsNullOrEmpty(Port.Text))
+            {
+                MessageBox.Show("Port musí být vyplněn!");
+                return;
+            }
+
+            int test = 0;
+            if (!Int32.TryParse(Port.Text, out test))
+            {
+                MessageBox.Show("Port musí číslo!");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Odesilatel.Text))
+            {
+                MessageBox.Show("Odesílatel musí být vyplněn!");
+                return;
+            }
+
+            if (!IsValidEmail(Odesilatel.Text))
+            {
+                MessageBox.Show("Neplatný e-mail odesílatele!");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Prijemce.Text))
+            {
+                MessageBox.Show("Příjemce musí být vyplněn!");
+                return;
+            }
+
+            if (!IsValidEmail(Prijemce.Text))
+            {
+                MessageBox.Show("Neplatný e-mail příjemce!");
+                return;
+
+            }
+
+            if (string.IsNullOrEmpty(Predmet.Text))
+            {
+                MessageBox.Show("Předmět musí být vyplněn");
+                return;
+            }
+            if (string.IsNullOrEmpty(TextZpravy.Text))
+            {
+                MessageBox.Show("Zpráva musí být vyplněna!");
+                return;
+            }
+
+
+            try
+            {
+                MailMessage message = new MailMessage(Odesilatel.Text, Prijemce.Text, Predmet.Text, TextZpravy.Text);
+
+                SmtpClient client = new SmtpClient(SMTP.Text, int.Parse(Port.Text));
+                client.EnableSsl = SSL.Checked;
+                client.Send(message);
+                MessageBox.Show("E-mail byl odeslán", "E-mail byl odeslán", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Chyba při odeslání e-mailu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+
+    class IniFile
+    {
+        private readonly string _path;
+
+        public IniFile(string path)
+        {
+            _path = path;
+        }
+
+        public string Read(string key, string section = null)
+        {
+            var stringBuilder = new StringBuilder(255);
+            GetPrivateProfileString(section ?? "", key, "", stringBuilder, 255, _path);
+            return stringBuilder.ToString();
+        }
+
+        public void Write(string key, string value, string section = null)
+        {
+            WritePrivateProfileString(section ?? "", key, value, _path);
+        }
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string value, string path);
+
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string defaultValue, StringBuilder value, int size, string path);
+    }
+}
